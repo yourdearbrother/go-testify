3a5fd5b9dcf3adfe82bb9d6acf259d013312d1af
Roman Zinatulin <zrf_123@mail.ru>
2024-04-23 02:05:55 +0300

    work done

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
     "net/http"
-    "net/http/httptest"
     "strconv"
     "strings"
-    "testing"
 )
 
 var cafeList = map[string][]string{
@@ -44,15 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
     w.WriteHeader(http.StatusOK)
     w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
-
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
-
-    // здесь нужно добавить необходимые проверки
 }
---
+++ main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerCheckAnswerAndBody(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=3&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.NotEmpty(t, responseRecorder.Body.String())
+}
+
+func TestMainHandlerCity(t *testing.T)  {
+	req := httptest.NewRequest("GET", "/cafe?count=3&city=london", nil)
+
+	responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
+
+    require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+    assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+    totalCount := 4
+    req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+    responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+    list := strings.Split(body, ",")
+
+	assert.Len(t, list, totalCount)
+}
